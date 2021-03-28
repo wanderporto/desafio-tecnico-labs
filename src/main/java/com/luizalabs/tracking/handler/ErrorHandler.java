b9c2d305eaca48e78d40eb476beced63b7cc5e1f
wander.porto <wanderwang@hotmail.com.br>
2021-03-28 17:32:44 -0300

    add error exception not found

--- src/main/java/com/luizalabs/tracking/handler/ErrorHandler.java
+++ src/main/java/com/luizalabs/tracking/handler/ErrorHandler.java
@@ -1,15 +1,11 @@
 package com.luizalabs.tracking.handler;
 
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import com.luizalabs.tracking.execption.ScheduleNotFound;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,19 +19,41 @@ public class ErrorHandler{
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public ResponseEntity<?> handlerValidationExceptions(MethodArgumentNotValidException ex) {
-		Map<String, Object> body = new LinkedHashMap<>();
-        body.put("timestamp", Instant.now().toString());
-        body.put("status", HttpStatus.BAD_REQUEST);
-
-        //Get all errors
+		
         List<String> errors = ex.getBindingResult()
                 .getFieldErrors()
                 .stream()
                 .map(x -> x.getDefaultMessage())
                 .collect(Collectors.toList());
 
-        body.put("errors", errors);
 
-		return new ResponseEntity(body, HttpStatus.BAD_REQUEST);
+        return error(errors,HttpStatus.BAD_REQUEST);
 	}
+
+    @ExceptionHandler(ScheduleNotFound.class)
+	public ResponseEntity<?> handlerScheduleNotFoundExceptions(ScheduleNotFound ex) {
+		return error(ex.getMessage(), HttpStatus.NOT_FOUND);
+	}
+
+    public ResponseEntity internalServerError(String data) {
+        return error(data, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    public ResponseEntity error(String data, HttpStatus httpStatus) {
+        Map<Object, Object> model = new LinkedHashMap<>();
+        model.put("msg", httpStatus.getReasonPhrase());
+        model.put("status", httpStatus.value());
+        model.put("data", data);
+
+        return new ResponseEntity(model, httpStatus);
+    }
+
+    public ResponseEntity error(List<String> data, HttpStatus httpStatus) {
+        Map<Object, Object> model = new LinkedHashMap<>();
+        model.put("msg", httpStatus.getReasonPhrase());
+        model.put("status", httpStatus.value());
+        model.put("data", data);
+
+        return new ResponseEntity(model, httpStatus);
+    }
 }
